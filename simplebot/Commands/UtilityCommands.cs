9570404232829aed7dff55fbf6fea2ff93e8c335
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-22 20:21:31 +0200

    Created "ping" command

--- simplebot/Commands/UtilityCommands.cs
+++ simplebot/Commands/UtilityCommands.cs
@@ -1,5 +1,18 @@
-﻿namespace simplebot.Commands; 
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace simplebot.Commands; 
 
 public class UtilityCommands {
     
+    [Command("ping")] 
+    public async Task PingAsync(CommandContext ctx) {
+        var embed = new DiscordEmbedBuilder {
+            Description = $"Pong! Your current ping is: {ctx.Client.Ping}ms",
+            Color = DiscordColor.Green
+        };
+
+        await ctx.Channel.SendMessageAsync(embed);
+    }
 }
