e3e59227939655f042f423b97b7b3f1cb9c45c0b
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-27 13:30:11 +0200

    Added ban, tempban and unban

--- simplebot/Commands/ModerationCommand.cs
+++ simplebot/Commands/ModerationCommand.cs
@@ -41,4 +41,55 @@ public class ModerationCommand : BaseCommandModule {
         await member.RemoveAsync(reason);
         await ctx.Channel.SendMessageAsync(embed);
     }
+    
+    [Command("ban"), System.ComponentModel.Description("Bans a specified user.")]
+    public async Task BanAsync(CommandContext ctx, DiscordMember member, [RemainingText] string reason = "No reason provided.") {
+        if (!ctx.Member.PermissionsIn(ctx.Channel).HasPermission(Permissions.BanMembers)) {
+            await ctx.Channel.SendMessageAsync("You don't have permission to use this command!");
+            return;
+        }
+
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle(":white_check_mark: Success!")
+            .WithDescription($"Succesfully banned {member.Mention}!\nReason: {reason}!")
+            .WithColor(DiscordColor.Green)
+            .WithTimestamp(DateTime.Now);
+        
+        await member.BanAsync(0, reason);
+        await ctx.Channel.SendMessageAsync(embed);
+    }
+    
+    [Command("tempban"), System.ComponentModel.Description("Temporarily bans a specified user.")]
+    public async Task TempbanAsync(CommandContext ctx, DiscordMember member, int time, [RemainingText] string reason = "No reason provided.") {
+        if (!ctx.Member.PermissionsIn(ctx.Channel).HasPermission(Permissions.BanMembers)) {
+            await ctx.Channel.SendMessageAsync("You don't have permission to use this command!");
+            return;
+        }
+
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle(":white_check_mark: Success!")
+            .WithDescription($"Succesfully temporarily banned {member.Mention}!\nReason: {reason}!")
+            .WithColor(DiscordColor.Green)
+            .WithTimestamp(DateTime.Now);
+        
+        await member.BanAsync(time, reason);
+        await ctx.Channel.SendMessageAsync(embed);
+    }
+
+    [Command("unban"), System.ComponentModel.Description("Unbans a specified user.")]
+    public async Task UnbanAsync(CommandContext ctx, DiscordUser user, [RemainingText] string reason = "No reason provided.") {
+        if (!ctx.Member.PermissionsIn(ctx.Channel).HasPermission(Permissions.BanMembers)) {
+            await ctx.Channel.SendMessageAsync("You don't have permission to use this command!");
+            return;
+        }
+
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle(":white_check_mark: Success!")
+            .WithDescription($"Succesfully unbanned {user.Mention}!\nReason: {reason}!")
+            .WithColor(DiscordColor.Green)
+            .WithTimestamp(DateTime.Now);
+        
+        await ctx.Guild.UnbanMemberAsync(user, reason);
+        await ctx.Channel.SendMessageAsync(embed);
+    }
 }
