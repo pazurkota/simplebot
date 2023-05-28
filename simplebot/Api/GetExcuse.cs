f53e059256eed6830278511eb9b4a38a2f4446c5
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-28 13:41:28 +0200

    Creadted "GetExcuse" class

--- simplebot/Api/GetExcuse.cs
+++ simplebot/Api/GetExcuse.cs
@@ -1,5 +1,13 @@
-﻿namespace simplebot.Api; 
+﻿using simplebot.Classes;
 
-public class GetExcuse {
-    
+namespace simplebot.Api; 
+
+public class GetExcuse : IApiRequest {
+    public string GetRequest() {
+        throw new NotImplementedException();
+    }
+
+    public ExcuseClass ParseData<ExcuseClass>(string data) {
+        throw new NotImplementedException();
+    }
 }
--- simplebot/Classes/ExcuseClass.cs
+++ simplebot/Classes/ExcuseClass.cs
@@ -1,5 +1,7 @@
 ﻿namespace simplebot.Classes; 
 
 public class ExcuseClass {
-    
+    public int Id { get; set; }
+    public string Excuse { get; set; } = null!;
+    public string Category { get; set; } = null!;
 }
