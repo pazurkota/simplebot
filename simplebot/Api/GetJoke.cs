df5ef51904806cfcde9c2c23bded7ca21cd65f64
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-05-28 16:08:38 +0200

    Get random joke from API

---
+++ simplebot/Api/GetJoke.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace simplebot.Api; 
+
+public class GetJoke : ApiRequest {
+    private const string BaseUrl = "https://api.api-ninjas.com/v1/";
+    
+    protected override string GetRequest() {
+        try {
+            var apiKey = ConfigHandler.GetConfig().ApiNinjasApiKey;
+            
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("jokes?limit=1").AddHeader("X-Api-Key", apiKey);
+            
+            var response = client.Get(request).Content;
+            
+            if (response == null) {
+                throw new Exception("Response is null");
+            }
+            
+            return response;
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public override List<T> ParseData<T>() {
+        try {
+            var data = GetRequest();
+            var joke = JsonConvert.DeserializeObject<List<T>>(data);
+
+            if (joke == null) {
+                throw new Exception("Joke is null");
+            }
+            
+            return joke;
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
