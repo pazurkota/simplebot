23f22e29c2790f1f8736019b40d8b37af568ecfa
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-08 10:38:17 +0200

    Created Insult class

---
+++ simplebot/Classes/InsultClass.cs
@@ -0,0 +1,8 @@
+using Newtonsof.Json;
+
+namespace simplebot.Classes;
+
+public class InsultClass {
+    [JsonProperty("insult")] public string Insult { get; set; } = null!;
+    [JsonProperty("createdby")] public string Author { get; set; } = null!;
+}
