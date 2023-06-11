3f102d53906fe55f716a50df14da529bf380380f
pazurkota <68638349+pazurkota@users.noreply.github.com>
2023-06-11 12:52:02 +0000

    Created DiscordUser.cs

---
+++ simplebot/Engine/LevelEngine/DiscordUser.cs
@@ -0,0 +1,7 @@
+namespace simplebot.LevelEngine;
+
+public class DiscordUser {
+    public string Username { get; set; } = null!;
+    public int Xp { get; set; }
+    public int Level { get; set; }
+}
