4e5a5b0d446bbc54203ffbe167b98ef17a335a62
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-12 13:33:48 +0200

    Created StoreUserDetails()

--- simplebot/Engine/LevelEngine/Engine.cs
+++ simplebot/Engine/LevelEngine/Engine.cs
@@ -1,5 +1,23 @@
-﻿namespace simplebot.Engine.LevelEngine; 
+﻿using Newtonsoft.Json.Linq;
+
+namespace simplebot.Engine.LevelEngine; 
 
 public class Engine {
-    
+    public bool StoreUserDetails(DiscordUser user) {
+        try {
+            string path = "Engine/LevelEngine/userinfo.json";
+
+            string json = File.ReadAllText(path);
+            JObject jsonObj = JObject.Parse(json);
+
+            var members = jsonObj["members"].ToObject<List<DiscordUser>>();
+            File.WriteAllText(path, jsonObj.ToString());
+
+            return true; // return true if the user was successfully stored
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            return false; // return false if the user was not successfully stored
+        }
+    }
 }
