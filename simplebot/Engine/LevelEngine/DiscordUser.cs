a2f0f4b70eca0266d7ce43c05ca14f4a31a6f7d2
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-12 13:28:56 +0200

    Edited DiscordUser.cs and created Engine.cs

--- simplebot/Engine/LevelEngine/DiscordUser.cs
+++ simplebot/Engine/LevelEngine/DiscordUser.cs
@@ -1,7 +1,9 @@
-namespace simplebot.LevelEngine;
+namespace simplebot.Engine.LevelEngine;
 
 public class DiscordUser {
     public string Username { get; set; } = null!;
+    public string GuildId { get; set; } = null!;
+    public string AvatarUrl { get; set; } = null!;
     public int Xp { get; set; }
     public int Level { get; set; }
 }
