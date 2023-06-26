97c00a19d566c56c19ad94a224e20ab8e062278d
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-26 13:44:43 +0200

    Created "Pause" and "Resume" commands

--- simplebot/Commands/MusicCommands.cs
+++ simplebot/Commands/MusicCommands.cs
@@ -15,15 +15,18 @@ public class MusicCommands : ApplicationCommandModule{
         
         if (ctx.Member.VoiceState == null || user == null)
         {
-            await ctx.Channel.SendMessageAsync("Please enter a VC!!!");
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
         }
         
         if (!lavalinkInstance.ConnectedNodes.Any()) {
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Lavalink node is not connected!"));
+            return;
         }
 
         if (user.Type != ChannelType.Voice) {
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
         }
         
         var node = lavalinkInstance.ConnectedNodes.Values.First();
@@ -32,11 +35,13 @@ public class MusicCommands : ApplicationCommandModule{
         var connection = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
         if (connection == null) {
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Failed to connect to voice channel!"));
+            return;
         }
         
         var searchQuery = await node.Rest.GetTracksAsync(song);
         if (searchQuery.LoadResultType == LavalinkLoadResultType.NoMatches || searchQuery.LoadResultType == LavalinkLoadResultType.LoadFailed) {
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"No matches found for : {song}"));
+            return;
         }
 
         var musicTrack = searchQuery.Tracks.First();
@@ -45,7 +50,7 @@ public class MusicCommands : ApplicationCommandModule{
 
         var embed = new DiscordEmbedBuilder() {
             Title = "Now playing:",
-            Description = $":notes: {musicTrack.Title} by {musicTrack.Author}\n" +
+            Description = $"{musicTrack.Title} by {musicTrack.Author}\n" +
                           $"Duration: {musicTrack.Length}\n" +
                           $"Requested by: {ctx.Member.Username}\n" +
                           $"Url: [Click here!]({musicTrack.Uri})",
@@ -53,6 +58,102 @@ public class MusicCommands : ApplicationCommandModule{
             Timestamp = DateTime.Now
         };
 
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
+    }
+
+    [SlashCommand("pause", "Pause the current song")]
+    public async Task PauseMusicAsync(InteractionContext ctx) {
+        await ctx.DeferAsync();
+        
+        var lavalinkInstance = ctx.Client.GetLavalink();
+        var user = ctx.Member.VoiceState.Channel;
+        
+        if (ctx.Member.VoiceState == null || user == null)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
+        }
+        
+        if (!lavalinkInstance.ConnectedNodes.Any()) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Lavalink node is not connected!"));
+            return;
+        }
+
+        if (user.Type != ChannelType.Voice) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
+        }
+        
+        var node = lavalinkInstance.ConnectedNodes.Values.First();
+        var connection = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+        
+        if (connection == null) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Failed to connect to voice channel!"));
+            return;
+        }
+
+        if (connection.CurrentState.CurrentTrack == null) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("There is no song playing!"));
+            return;
+        }
+        
+        await connection.PauseAsync();
+
+        var embed = new DiscordEmbedBuilder() {
+            Title = "Stopped playing:",
+            Description = $"{connection.CurrentState.CurrentTrack.Title} by {connection.CurrentState.CurrentTrack.Author}\n",
+            Color = DiscordColor.Red,
+            Timestamp = DateTime.Now
+        };
+            
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
+    }
+    
+    [SlashCommand("resume", "Resumes the current song")]
+    public async Task ResumeMusicAsync(InteractionContext ctx) {
+        await ctx.DeferAsync();
+        
+        var lavalinkInstance = ctx.Client.GetLavalink();
+        var user = ctx.Member.VoiceState.Channel;
+        
+        if (ctx.Member.VoiceState == null || user == null)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
+        }
+        
+        if (!lavalinkInstance.ConnectedNodes.Any()) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Lavalink node is not connected!"));
+            return;
+        }
+
+        if (user.Type != ChannelType.Voice) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You must be in a voice channel!"));
+            return;
+        }
+        
+        var node = lavalinkInstance.ConnectedNodes.Values.First();
+        var connection = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+        
+        if (connection == null) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Failed to connect to voice channel!"));
+            return;
+        }
+
+        if (connection.CurrentState.CurrentTrack == null) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("There is no song playing!"));
+            return;
+        }
+        
+        await connection.ResumeAsync();
+
+        var embed = new DiscordEmbedBuilder() {
+            Title = "Resumed playing:",
+            Description = $"{connection.CurrentState.CurrentTrack.Title} by {connection.CurrentState.CurrentTrack.Author}\n",
+            Color = DiscordColor.Green,
+            Timestamp = DateTime.Now
+        };
+            
         await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
     }
 }
