36cb75e9bc35f81e3564e7eec7ea27413fab7b2e
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-09-04 11:10:29 +0200

    FIXED: User checks

--- simplebot/Commands/MusicCommands.cs
+++ simplebot/Commands/MusicCommands.cs
@@ -6,5 +6,23 @@ using DSharpPlus;
 namespace simplebot.Commands; 
 
 public class MusicCommands : ApplicationCommandModule{
-    
+    [SlashCommand("play", "Plays a specified music")]
+    public async Task PlayMusicAsync(InteractionContext ctx, [Option("music", "URL of this song")] string musicUrl) {
+        await ctx.DeferAsync();
+        
+        var user = ctx.Member.VoiceState;
+        var lavalinkInstance = ctx.Client.GetLavalink();
+        
+        if (user == null! || user.Channel.Type != ChannelType.Voice) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("You are not in a voice channel!"));
+            return;
+        }
+        
+        if (!lavalinkInstance.ConnectedNodes.Any()) {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Lavalink is not connected!"));
+            return;
+        }
+        
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Command response! (music: {musicUrl})"));
+    }
 }
